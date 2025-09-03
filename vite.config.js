5dcec330ccb290bcb1d1e394e3f5320b85b1bef9
MohamedNHagag <Mohamedhaga9999@gmail.com>
2025-09-03 22:27:58 +0300

    fix deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/',
   plugins: [react()],
-  base: "./",   // 👈 دي اللي هتخلي الـ assets والـ js/css يتحملوا صح
 })
